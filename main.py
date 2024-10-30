bb202c9804eede0b8145b4de09660ce463cf5407
Lino Schopp <lino.elias.schopp@icloud.com>
2024-10-30 20:50:51 +0100

    argparse:     -n --ntfy:         select devices     -b --batch-size.         select batch_size
    ADDED

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import os
 os.environ["KERAS_BACKEND"] = "tensorflow"
 
 import keras
-from keras.layers import Input, Dense, Conv2D, MaxPooling2D, Dropout, Flatten, BatchNormalization # noqa
-from keras.models import Sequential # noqa
+from keras.layers import Input, Dense, Conv2D, MaxPooling2D, Dropout, Flatten, BatchNormalization  # noqa
+from keras.models import Sequential  # noqa
 import requests
 import datetime
 import models
@@ -13,14 +13,20 @@ import argparse
 parser = argparse.ArgumentParser(description="Train an image generation model")
 
 parser.add_argument("-e", "--epochs",
-                    required=True, type=int, help="Number of epochs the model will be trained with")
-parser.add_argument("-n", "--notify",
-                    )
+                    required=True, type=int,
+                    help="Number of epochs the model will be trained with")
+parser.add_argument("-n", "--ntfy",
+                    choices=["none", "ipad", "iphone", "both"], default="none",
+                    help="Which devices to ntfy")
+parser.add_argument("-b", "--batch-size",
+                    type=int, default=128,
+                    help="Size of the batches the model will be trained with")
+# parser.add_argument("")
+
 
 args = parser.parse_args()
 
 epochs = args.epochs
-print(f"epochs: {epochs}")
 
 (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
 
@@ -32,6 +38,12 @@ y_test = keras.utils.to_categorical(y_test, 10)
 took = None
 
 
+def get_topic(arg):
+    if args.ntfy.startswith("i"):
+        return "lino-" + args.ntfy
+    return "linoschopp"
+
+
 class CustomCallback(keras.callbacks.Callback):
     def __init__(self):
         self.start = None
@@ -41,6 +53,8 @@ class CustomCallback(keras.callbacks.Callback):
         self.start = datetime.datetime.now()
 
     def on_epoch_end(self, epoch, logs=None):
+        if epoch == epochs - 1:
+            return
         done = datetime.datetime.now() - self.start
         time_per_epoch = done / (epoch + 1)
         remaining_epochs = epochs - (epoch + 1)
@@ -48,10 +62,11 @@ class CustomCallback(keras.callbacks.Callback):
         finish_time = datetime.datetime.now() + remaining_time
         if (finish_time.hour, finish_time.minute) != self.last_predicted_time:
             self.last_predicted_time = (finish_time.hour, finish_time.minute)
-            requests.post("https://ntfy.sh/linoschopp",
-                          data=f"Done at: {finish_time.hour:02d}:{finish_time.minute:02d}",
-                          headers={"Title": f"Training: {((epoch + 1) / epochs):.1%}"}
-                          )
+            if args.ntfy != "none":
+                requests.post(f"https://ntfy.sh/{get_topic(args.ntfy)}",
+                              data=f"Done at: {finish_time.hour:02d}:{finish_time.minute:02d}",
+                              headers={"Title": f"Training: {((epoch + 1) / epochs):.1%}"}
+                              )
 
     def on_train_end(self, logs=None):
         global took
@@ -77,11 +92,12 @@ model.compile(
     metrics=["accuracy"]
 )
 
-model.fit(x=x_train, y=y_train, epochs=epochs, batch_size=128)  #, callbacks=[CustomCallback()], verbose=1)
+model.fit(x=x_train, y=y_train, epochs=epochs, batch_size=args.batch_size, callbacks=[CustomCallback()], verbose=1)
 loss, acc = model.evaluate(x=x_test, y=y_test, verbose=1)
-#requests.post("https://ntfy.sh/linoschopp",
-#                  data=f"Took: {took}\nAccuracy: {acc:%}",
-#                  headers={"Title":f"Finished training"}
-#             )
+if args.ntfy != "none":
+    requests.post(f"https://ntfy.sh/{get_topic(args.ntfy)}",
+                  data=f"Took: {took}\nAccuracy: {acc:%}",
+                  headers={"Title": f"Finished training"}
+                  )
 print(f"Model accuracy: {acc:%}")
 models.ask_for_save(model)
